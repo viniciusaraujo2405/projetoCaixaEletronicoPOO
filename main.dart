6a63bb1511f43875d60f3ffacf2640ac7b2b9baa
Vinícius Araújo <d2022003334@unifei.edu.br>
2024-12-01 15:19:41 -0300

    Criando instâncias como exemplos para testar as funcionalidades das classes

--- main.dart
+++ main.dart
@@ -4,6 +4,9 @@ import 'conta.dart';
 main(){
 
   Conta conta = Conta(nome: 'Vinícius', saldo: 1000.0);
+  Conta conta2 = Conta(nome: 'Gaita', saldo: 0);
   CaixaEletronico caixa = CaixaEletronico(conta);
+  CaixaEletronico caixa2 = CaixaEletronico(conta2);
   caixa.iniciar();
+  caixa2.iniciar();
 }
