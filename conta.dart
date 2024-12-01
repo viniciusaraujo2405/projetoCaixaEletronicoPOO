47bd37bf224e5044a92d7277d1c2b7c24db68cca
Vinícius Araújo <d2022003334@unifei.edu.br>
2024-12-01 14:12:19 -0300

    Implementando os métodos da classe conta

--- conta.dart
+++ conta.dart
@@ -3,9 +3,22 @@ class Conta{
   double saldo;
 
   Conta({required this.nome, this.saldo = 0});
-  void depositar(double valor)
-  bool sacar(double valor)
-  void exibirSaldo()
+  void depositar(double valor){
+    saldo = saldo + valor;
+  }
+  bool sacar(double valor){
+    if (saldo>=valor){
+      saldo = saldo - valor;
+      print("Saque de $valor realizado com sucesso!!");
+      return true;
+    }else{
+      print("Saldo insuficiente!");
+      return false;
+    }
+  }
+  void exibirSaldo(){
+    print('Titular: $nome, Saldo: $saldo');
+  }
 
   
 
