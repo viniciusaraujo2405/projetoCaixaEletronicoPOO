4639b514ca4a52f19786cccf7d62cf2877dc412a
Vinícius Araújo <d2022003334@unifei.edu.br>
2024-12-01 14:09:21 -0300

    Definindo os atributos e os metódos

---
+++ conta.dart
@@ -0,0 +1,13 @@
+class Conta{
+  String nome;
+  double saldo;
+
+  Conta({required this.nome, this.saldo = 0});
+  void depositar(double valor)
+  bool sacar(double valor)
+  void exibirSaldo()
+
+  
+
+
+}
