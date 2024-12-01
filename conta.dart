6023cc7750deb750861a3d75467727cfd58ff220
Vinícius Araújo <d2022003334@unifei.edu.br>
2024-12-01 15:10:03 -0300

    Criando uma nova funcionalidade: Transferencia

--- conta.dart
+++ conta.dart
@@ -20,6 +20,17 @@ class Conta{
     print('Titular: $nome, Saldo: $saldo');
   }
 
+  bool transferir(Conta contaDestino, double valor){
+    if(sacar(valor)){
+      contaDestino.depositar(valor);
+      print('Transferência de R\$${valor.toStringAsFixed(2)} para ${contaDestino.nome} realizada com sucesso!');
+      return true;
+    }else{
+      print('Transferência falhou, saldo insuficiente.');
+      return false;
+    }
+  }
+
   
 
 
