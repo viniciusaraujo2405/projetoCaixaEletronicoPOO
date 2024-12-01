39f369c2c1926059b10e7a9b2004dd636d3d2f50
Vinícius Araújo <d2022003334@unifei.edu.br>
2024-12-01 15:30:55 -0300

    Finalizando

--- main.dart
+++ main.dart
@@ -2,7 +2,6 @@ import 'caixa.dart';
 import 'conta.dart';
 
 main(){
-
   Conta conta = Conta(nome: 'Vinícius', saldo: 1000.0);
   Conta conta2 = Conta(nome: 'Gaita', saldo: 0);
   CaixaEletronico caixa = CaixaEletronico(conta);
